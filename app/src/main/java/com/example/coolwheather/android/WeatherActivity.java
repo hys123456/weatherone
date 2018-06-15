e25facb1feb228bffe45d86ac7f46ec2fbd159a5
hys123456 <572757886@QQ.com>
2018-06-15 15:43:04 +0800

    增加后台自动更新天气gonn

--- app/src/main/java/com/example/coolwheather/android/WeatherActivity.java
+++ app/src/main/java/com/example/coolwheather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.coolwheather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -214,6 +215,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startActivity(intent);
     }
 
 }
